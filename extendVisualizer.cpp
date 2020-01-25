47a597b0c5b64b6d78d297efcaac0b5cd6078fd8
Aravind-Sundararajan <asundar4@vols.utk.edu>
2020-01-24 22:04:19 -0500

    passing CPtr to mex works just fine

--- extendVisualizer.cpp
+++ extendVisualizer.cpp
@@ -1,6 +1,6 @@
 #include <Simbody.h>
 #include <OpenSim.h>
-#include "C:/Users/asundar4/Documents/libs/include/mexplus.h"
+#include "C:\Users\aravi\Documents\MATLAB\mexplus-master\include\mexplus.h"
 #include <vector>
 using namespace std;
 using namespace mexplus;
@@ -8,24 +8,31 @@ using namespace OpenSim;
 using namespace SimTK;
 class extendVisualizer {
 public:
-	extendVisualizer(const int cPtr){
-        
-		mexPrintf("Calling constructor.\n");
-        m_visualizer = (SimTK::Visualizer *)cPtr;
-        mexPrintf("Ground Height:'%s'.\n", m_visualizer->getMode());
+	extendVisualizer(const __int64 cPtr){        
+  mexPrintf("Calling constructor.\n");
+  m_visualizer = (ModelVisualizer *)cPtr;
+  simbodyVisualizer = &(m_visualizer->updSimbodyVisualizer());
+  simbodyVisualizer->setShowSimTime(false);
+  simbodyVisualizer->setShowFrameNumber(false);
+  simbodyVisualizer->setBackgroundType(SimTK::Visualizer::BackgroundType(2));
+  simbodyVisualizer->setBackgroundColor(SimTK::Vec3(0,0,0));
+  mexPrintf("Model:'%s'.\n", (m_visualizer->getModel()).getName());
 	}
 	virtual ~extendVisualizer(){
 		mexPrintf("Calling destructor.\n");
+  delete m_visualizer;
+  delete simbodyVisualizer;
 	}
 private:
-	ReferencePtr<SimTK::Visualizer> m_visualizer;
+	ModelVisualizer* m_visualizer = NULL;
+ SimTK::Visualizer* simbodyVisualizer = NULL;
 };
 template class mexplus::Session<extendVisualizer>;
 namespace {
 	MEX_DEFINE(new) (int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[]){
 		InputArguments input(nrhs, prhs, 1);
 		OutputArguments output(nlhs, plhs, 1);
-		output.set(0, Session<extendVisualizer>::create(new extendVisualizer(input.get<int>(0))));
+		output.set(0, Session<extendVisualizer>::create(new extendVisualizer(input.get<__int64>(0))));
 	}
 	MEX_DEFINE(delete) (int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[]){
 		InputArguments input(nrhs, prhs, 1);
